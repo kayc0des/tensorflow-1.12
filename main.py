2db3dce38392acc30d707ef88b9553b4fe60c018
kayc0des <k.boafo@alustudent.com>
2024-05-27 14:18:06 +0000

    forward prop

--- main.py
+++ main.py
@@ -43,9 +43,70 @@ def create_layer(prev, n, activation):
     return output
 
 
+def forward_prop(x, layer_sizes=[], activations=[]):
+    '''
+    Creates the forward propagation graph
+
+    Param:
+    x -> placeholder fot the input data
+    layer_sizes -> list containing the number of nodes of the network
+    activations -> list containing activation functions for each layer of the network
+
+    Returns:
+    Prediction for the network in tensor form
+    '''
+    
+    # initialize dictionarie's to hold values of z and a
+    output_z = {}
+    ouput_a = {}
+    layers = {}
+
+    with tf.Session as sess:
+        for i in range(len(layer_sizes):
+                if i == 0:
+                    layer[i + 1] = create_layer(x, layer_sizes[i], activations[i])     
+                    output_z['z' + str(i + 1)] = tf.add(tf.matmul(x, layer[i + 1].get_variable('dense/kernel')))
+                    output_a['a' + str(i + 1)] = activations[i](output['z' + str(i + 1)])
+                else:
+                    # to do
+
+    
+
+
 # debug
 x, y = create_placeholders(784, 10)
 print(x)
 print(y)
 l = create_layer(x, 256, tf.nn.tanh)
 print(l)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
